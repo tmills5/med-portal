eb7bb77b07309cf8f55762f895fff6d27ea45614
Merle Tyson Mills <tysonmills5@gmail.com>
2023-01-03 21:18:52 -0600

    seedin work

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [patients, setPatients] = useState({});
+
+  useEffect( ()=> {
+    fetch('/patients')
+    .then(response => {
+      if (response.ok) {
+        response.json()
+        .then(data=> {
+          console.log(data)
+          setPatients(data)
+        });
+      }
+    })
+    .catch(err => console.error(err));
+  },[]);
+
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + dfgReact</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <ul>
+        {data}
+      </ul> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
